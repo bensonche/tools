6334d0817e40e6014c13d8e39f6ed00cde3d04a5
Benson Che <bche@resdat.com>
2013-04-22 15:25:58 -0800

    Copy intranet settings to separate file

--- setup.bash
+++ setup.bash
@@ -1,1 +1,9 @@
-cp bashrc.bash ~/.bashrc
+#!/bin/bash
+
+grep -q intranet_settings ~/.bashrc
+if [ $? -ne 0 ]
+then
+	echo -e "\n\n.intranet_settings" >> ~/.bashrc
+fi
+
+cp bashrc.bash ~/.intranet_settings
