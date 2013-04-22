0a5b0e3ce351f490529bb0dce39a0f58a99c3157
Benson Che <bche@resdat.com>
2013-04-22 15:59:27 -0800

    Revert to overwrite bashrc

--- setup.bash
+++ setup.bash
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-grep -q intranet_settings ~/.bashrc
-if [ $? -ne 0 ]
-then
-	echo -e "\n\n.intranet_settings" >> ~/.bashrc
-fi
+#grep -q intranet_settings ~/.bashrc
+#if [ $? -ne 0 ]
+#then
+#	echo -e "\n\n./.intranet_settings" >> ~/.bashrc
+#fi
 
-cp bashrc.bash ~/.intranet_settings
+#cp bashrc.bash ~/.intranet_settings
+
+cp bashrc.bash ~/.bashrc
