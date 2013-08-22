4245d917572ec3f84d4ef5ff1e2e39cd25c89812
Benson Che <bche@resdat.com>
2013-08-22 10:53:29 -0800

    Add log.bash

---
+++ log.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+BRANCH=release
+
+git checkout $1
+if [ $? -ne 0 ]
+then
+	echo "Error checking out"
+	exit 1
+fi
+git pull origin $1
+
+echo $1 >> log.txt
+git log --left-right --cherry-pick --pretty=format:"%ad, %aN: %s" $BRANCH..head >> log.txt
+echo -e "\n" >> log.txt
