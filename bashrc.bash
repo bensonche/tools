31e09fe187c19428c5caef7d55b10b33889c8956
Benson Che <bche@resdat.com>
2013-10-25 12:08:30 -0700

    Update bashrc.bash

--- bashrc.bash
+++ bashrc.bash
@@ -7,6 +7,8 @@ function create_aliases {
 	alias gt="${TOOLS_DIR}tag.bash"
 	alias gr="git reset --hard && git clean -dfx"
 	alias pt="${TOOLS_DIR}push_to_test.bash"
+	alias db="${TOOLS_DIR}create_db_script.bash"
+	alias log="${TOOLS_DIR}log.bash"
 }
 
 create_aliases
