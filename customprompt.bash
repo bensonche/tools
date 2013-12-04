20869b4ea7f0b9e9f30140b8df8b0a56b87359d5
Benson Che <bche@resdat.com>
2013-12-04 10:15:12 -0900

    Use __git_ps1 only if exists

--- customprompt.bash
+++ customprompt.bash
@@ -1,6 +1,3 @@
-bold="\033[1m"
-normal="\033[0m"
-
 smiley () {
 	if [ $? -eq 0 ]
 	then
@@ -10,4 +7,19 @@ smiley () {
 	fi
 }
 
-export PS1="\n\$(smiley) ${bold}\[\033[33m\]\w${normal}\[\033[0m\]\n$ "
+create_prompt () {
+	bold="\033[1m"
+	normal="\033[0m"
+
+	#check that __git_ps1 exists
+	local GIT_PS1=""
+	type __git_ps1 > /dev/null
+	if [ $? -eq 0 ]
+	then
+		GIT_PS1="\$(__git_ps1)"
+	fi
+
+	export PS1="\n\$(smiley) ${bold}\[\033[33m\]\w${GIT_PS1}${normal}\[\033[0m\]\n$ "
+}
+
+create_prompt
