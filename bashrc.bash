31e7cb1e992eb0e5cf8a013c6c4461bd01e06543
Benson Che <bche@resdat.com>
2013-12-04 09:52:46 -0900

    Add global git configs

--- bashrc.bash
+++ bashrc.bash
@@ -13,6 +13,21 @@ then
 	alias log="${TOOLS_DIR}log.bash"
 fi
 
-git config --global alias.gr "!git reset --hard && git clean -dfx"
+local GIT_CONF="git config --global"
 
+$GIT_CONF alias.gr "!git reset --hard && git clean -dfx"
+$GIT_CONF alias.cm commit
+$GIT_CONF alias.s status
+$GIT_CONF alias.l "log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold red)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%an - %s%C(reset)%C(bold yellow)%d%C(reset)'"
+$GIT_CONF alias.mt "mergetool --no-prompt"
+$GIT_CONF alias.m merge
+$GIT_CONF alias.dt difftool
+$GIT_CONF alias.d diff
+$GIT_CONF alias.a add
+$GIT_CONF alias.co checkout
+$GIT_CONF alias.mprod "merge --no-ff -sresolve"
+$GIT_CONF alias.po "!git pull origin $(git rev-parse --abbrev-ref HEAD)"
+$GIT_CONF push.default current
+$GIT_CONF color.status always
+	
 source ~/.custom_prompt
