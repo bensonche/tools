4b29c89530d0bd361f61489e5dd49655bf522c85
Benson Che <bche@resdat.com>
2014-02-05 15:06:20 -0900

    git settings

--- setup.bash
+++ setup.bash
@@ -27,6 +27,7 @@ bc_setup ()
 	then
 		local git_conf="git config --global"
 		$git_conf alias.gr "!git reset --hard && git clean -df"
+		$git_conf alias.grx "!git reset --hard && git clean -dfx --exclude='_Resharper*'"
 		$git_conf alias.cm commit
 		$git_conf alias.s status
 		$git_conf alias.l "log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold red)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%an - %s%C(reset)%C(bold yellow)%d%C(reset)'"
@@ -39,6 +40,7 @@ bc_setup ()
 		$git_conf alias.mprod "merge --no-ff -sresolve"
 		$git_conf alias.po "!git pull origin \$(git rev-parse --abbrev-ref HEAD)"
 		$git_conf push.default current
+		$git_conf rebase.autosquash true
 		$git_conf color.status always
 	fi
 
