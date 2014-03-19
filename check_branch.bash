278ee123b7cbce8feba390acaf37aa46de6c1350
Benson Che <bche@resdat.com>
2014-03-19 14:42:42 -0800

    Check w/o checking out the branch

--- check_branch.bash
+++ check_branch.bash
@@ -9,52 +9,59 @@ check_branch ()
 
 	local branch=release
 
-	git checkout $1
-	if [ $? -ne 0 ]
-	then
-		echo "Error checking out"
-		exit 1
-	fi
-	git pull origin $1
+	local right=$1
 
-	local updated=0
+	if [[ $1 != origin/* ]]
+	then
+		right=origin/$1
+	fi
+
+	#git checkout $right
+	#if [ $? -ne 0 ]
+	#then
+	#	echo "Error checking out"
+	#	exit 1
+	#fi
+	#git pull origin $right
+
+	#local updated=0
 
 	echo ""
 	if [ `git cherry -v head $branch | grep -c ""` -ne 0 ]
 	then
 		echo "Branch not updated"
 		echo ""
-		read -p "Press [Enter] key to auto-merge..."
-		git merge -sresolve $branch
-		
-		if [ $? -ne 0 ]
-		then
-			exit 1
-		fi
-		
-		updated=1
+		#read -p "Press [Enter] key to auto-merge..."
+		#git merge -sresolve $branch
+		#
+		#if [ $? -ne 0 ]
+		#then
+		#	exit 1
+		#fi
+		#
+		#updated=1
 	fi
 
 	echo -e "\e[0;32mList of commits in this branch:\e[00m"
-	#git cherry -v $branch head
-	git log --left-right --cherry-pick --pretty=format:"%ad, %aN: %s" $branch..head
+	#git cherry -v $branch $right
+	git log --left-right --cherry-pick --pretty=format:"%ad, %aN: %s" $branch..$right
 	echo ""
 	read -p "Press [Enter] key to continue..."
 
 	echo ""
 	echo -e "\e[0;32mList of files modified by this branch:\e[00m"
-	git diff --name-status $branch head
+	git diff --name-status $branch $right
 	echo ""
 	read -p "Press [Enter] key to continue..."
 
-	git difftool -w $branch..head
-	git diff -w $branch..head
+	git difftool -w $branch..$right
+	git diff -w $branch..$right
 
-	if [ $updated -ne 0 ]
-	then
-		read -p "Press [Enter] key to push branch"
-		git push origin $1
-	fi
+	#if [ $updated -ne 0 ]
+	#then
+	#	read -p "Press [Enter] key to push branch"
+	#	git push origin $right
+	#fi
 }
 
 check_branch $1
