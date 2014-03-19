f5e9ab94d6d7f19d2892ab897577574ea5df4abd
Benson Che <bche@resdat.com>
2014-03-19 14:43:55 -0800

    Add ac alias

--- setup.bash
+++ setup.bash
@@ -29,6 +29,7 @@ bc_setup ()
 		$git_conf alias.gr "!git reset --hard && git clean -df"
 		$git_conf alias.grx "!git reset --hard && git clean -dfx --exclude='_Resharper*'"
 		$git_conf alias.cm commit
+		$git_conf alias.ac "!git add -u && git commit"
 		$git_conf alias.s status
 		$git_conf alias.l "log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold red)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%an - %s%C(reset)%C(bold yellow)%d%C(reset)'"
 		$git_conf alias.ll "log --stat"
