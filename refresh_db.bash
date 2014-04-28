487bdce143808a7165328d7a044ef633e4ba32c6
Benson Che <bche@resdat.com>
2014-04-28 15:37:57 -0800

    Check args. Add flag to skip utf check.

--- refresh_db.bash
+++ refresh_db.bash
@@ -4,44 +4,60 @@ function create_db_script ()
 {
 	local sqlcmd_path="sqlcmd"
 	local start_ssms="start"
+	local silent=0
+	local skipCheck=0
+
+	for var in "$@"
+	do
+		case "$var" in
+			-s)
+				silent=1
+				;;
+			-c) skipCheck=1
+				;;
+		esac
+	done
 
 	if [ -f db_script.sql ]
 	then
 		rm db_script.sql
 	fi
 
-	echo "Checking files..."
-
-	local valid=1
-
-	find Database/repeatable -name \*.sql | egrep sql$ |
-	while read line; do
-		local file=$line
-		local fileWin=$(echo $file | sed 's/\//\\/g')
-		
-		grep -q ÿþ "$file"
-		if [ $? -eq 0 ]
-		then
-			valid=0
-			echo "$file is in UTF-16"
-
-			cmd //c type "$fileWin" > cb_temp_sql
-			cp cb_temp_sql "$file"
-		fi
-		grep -q ï»¿ "$file"
-		if [ $? -eq 0 ]
-		then
-			valid=0
-			echo "$file is in UTF-8 with BOM"
-
-			cmd //c type "$fileWin" > cb_temp_sql
-			cp cb_temp_sql "$file"
-		fi
-	done
-	
-	if [ $valid -eq 0 ]
+	if [ $skipCheck -eq 0 ]
 	then
-		exit 1
+		echo "Checking files..."
+
+		local valid=1
+
+		find Database/repeatable -name \*.sql | egrep sql$ |
+		while read line; do
+			local file=$line
+			local fileWin=$(echo $file | sed 's/\//\\/g')
+			
+			grep -q ÿþ "$file"
+			if [ $? -eq 0 ]
+			then
+				valid=0
+				echo "$file is in UTF-16"
+
+				cmd //c type "$fileWin" > cb_temp_sql
+				cp cb_temp_sql "$file"
+			fi
+			grep -q ï»¿ "$file"
+			if [ $? -eq 0 ]
+			then
+				valid=0
+				echo "$file is in UTF-8 with BOM"
+
+				cmd //c type "$fileWin" > cb_temp_sql
+				cp cb_temp_sql "$file"
+			fi
+		done
+	
+		if [ $valid -eq 0 ]
+		then
+			exit 1
+		fi
 	fi
 
 	echo "Creating script..."
@@ -53,7 +69,7 @@ function create_db_script ()
 		./db_files.txt
 	fi
 
-	if [ $# -gt 1 ] && [ $2 == "-s" ]
+	if [ $silent -eq 1 ]
 	then
 		exit 0
 	fi
