7f09e1745adffdb7e6393abf59763eed8ee451e0
Benson Che <bche@resdat.com>
2014-06-11 17:03:39 -0800

    Fix so it recreates button on postback

--- chrome/compare_button/compare.js
+++ chrome/compare_button/compare.js
@@ -1,27 +1,36 @@
 (function () {
-	var txtBranch = $("[id$=txtBranch]");
+	function buildButton () {
+		if($("a#githubCompare").length > 0) {
+			return;
+		}
+		var txtBranch = $("[id$=txtBranch]");
 
-	if(txtBranch.length == 0) {
-		return;
-	}
+		if(txtBranch.length == 0) {
+			return;
+		}
 
-	txtBranch.after("<a id='githubCompare' class='RDIHyperLink' href='#'>Compare</a>");
+		txtBranch.after("<a id='githubCompare' class='RDIHyperLink' href='#'>Compare</a>");
 
-	updateLink();
-
-	txtBranch.keyup(function () {
 		updateLink();
+
+		txtBranch.keyup(function () {
+			updateLink();
+		});
+
+
+		function getURL(branch) {
+			var github = "https://github.com/ResourceDataInc/Intranet/compare/";
+			return github + branch;
+		}
+
+		function updateLink() {
+			$("a#githubCompare").prop("href", getURL(txtBranch.val()));
+		}
+	}
+
+	buildButton();
+
+	document.addEventListener("DOMSubtreeModified", function(){
+		buildButton();
 	});
-
-
-	function getURL(branch) {
-		var github = "https://github.com/ResourceDataInc/Intranet/compare/";
-		return github + branch;
-	}
-
-	function updateLink() {
-		$("a#githubCompare").prop("href", getURL(txtBranch.val()));
-	}
-
-
 })()
