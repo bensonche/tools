51dc78cf8cad73810d15d1dd8711a28fe00e513b
Benson Che <bche@resdat.com>
2014-06-11 17:54:21 -0800

    Add QA button

--- chrome/compare_button/compare.js
+++ chrome/compare_button/compare.js
@@ -1,5 +1,42 @@
 (function () {
-	function buildButton () {
+	function buildQAButton() {
+		if(isRTP()) {
+			if($("input#QAButton").length > 0) {
+				return;
+			}
+
+			var assignTo = $("span#assignedToDdSpan");
+
+			if(assignTo.length == 0) {
+				return;
+			}
+
+			assignTo.after("<input type='button' id='QAButton' value='QA' class='RDIButton' />");
+
+			$("input#QAButton").click(function () {
+				if($("[id$=ddlAssignedTo]").val() != 10000) {
+					$("select[id$=ddlStatus] option[value=8]").prop("selected", true);
+					$("textarea[id$=txtComments]").val("In prod, please review.");
+
+					$("input[id$=Submit]").click();
+				}
+			});
+
+
+		} else {
+			$("input#QAButton").remove();
+		}
+	}
+
+
+	function isRTP() {
+		if($("[id$=ddlStatus]").length > 0) {
+			return $("[id$=ddlStatus]").val() == '48';
+		}
+		return false;
+	}
+
+	function buildCompareButton () {
 		if($("a#githubCompare").length > 0) {
 			return;
 		}
@@ -28,9 +65,12 @@
 		}
 	}
 
-	buildButton();
+	function init() {
+		buildCompareButton();
+		buildQAButton();
+	}
 
 	document.addEventListener("DOMSubtreeModified", function(){
-		buildButton();
+		init();
 	});
 })()
