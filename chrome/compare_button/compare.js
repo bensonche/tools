6ee388f54b106677b6e4d58a2f92e6f28871a0de
Benson Che <bche@resdat.com>
2014-06-11 16:24:26 -0800

    Add chrome extension to add compare button

---
+++ chrome/compare_button/compare.js
@@ -0,0 +1,27 @@
+(function () {
+	var txtBranch = $("[id$=txtBranch]");
+
+	if(txtBranch.length == 0) {
+		return;
+	}
+
+	txtBranch.after("<a id='githubCompare' class='RDIHyperLink' href='#'>Compare</a>");
+
+	updateLink();
+
+	txtBranch.keyup(function () {
+		updateLink();
+	});
+
+
+	function getURL(branch) {
+		var github = "https://github.com/ResourceDataInc/Intranet/compare/";
+		return github + branch;
+	}
+
+	function updateLink() {
+		$("a#githubCompare").prop("href", getURL(txtBranch.val()));
+	}
+
+
+})()
