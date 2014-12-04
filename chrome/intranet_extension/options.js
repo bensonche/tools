f88f32074249017ba15473c9a7d171f253dd4b74
Benson Che <bche@resdat.com>
2014-12-04 13:09:13 -0900

    Add options page

---
+++ chrome/intranet_extension/options.js
@@ -0,0 +1,23 @@
+function save_options() {
+	var empid = $("#txtID").val();
+	
+	chrome.storage.sync.set({
+		empid: empid
+	}, function () {
+		$("#Message").text("saved");
+		setTimeout(function() {
+			$("#Message").text("");
+		}, 500);
+	});
+}
+
+function restore_options() {
+	chrome.storage.sync.get({
+		empid: ''
+	}, function(items) {
+		$("#txtID").val(items.empid);
+	});
+}
+
+$(restore_options);
+$("#btnSave").click(save_options);
