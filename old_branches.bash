e5e7af07a0f81713d7a64aa83fae60f626b8f347
Benson Che <bche@resdat.com>
2014-12-16 10:29:02 -0900

    Rename created file

--- old_branches.bash
+++ old_branches.bash
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-echo "declare @git table" > create.sql
-echo "(" >> create.sql
-echo "    branch varchar(500)," >> create.sql
-echo "    id int" >> create.sql
-echo ")" >> create.sql
-echo "" >> create.sql
+echo "declare @git table" > branches.sql
+echo "(" >> branches.sql
+echo "    branch varchar(500)," >> branches.sql
+echo "    id int" >> branches.sql
+echo ")" >> branches.sql
+echo "" >> branches.sql
 
-echo "insert into @git" >> create.sql
-echo "values" >> create.sql
+echo "insert into @git" >> branches.sql
+echo "values" >> branches.sql
 
 git ls-remote --heads |
 	sed "s/.*refs\/heads\///" |
 	grep "[0-9][0-9][0-9][0-9][0-9]" |
 	sed "s/\(.*\([0-9][0-9][0-9][0-9][0-9]\).*\)/('\1', \2),/" |
-	sed "$ s/,$//" >> create.sql
+	sed "$ s/,$//" >> branches.sql
 
-echo "" >> create.sql
-echo "select '../tools/delete_if_merged.bash ' + c.branch, c.branch, 'git push origin :' + c.branch, c.id" >> create.sql
-echo "from rdiitem a" >> create.sql
-echo "    inner join itemstatus b" >> create.sql
-echo "        on a.StatusId = b.itemstatusid" >> create.sql
-echo "    inner join @git c" >> create.sql
-echo "        on a.rdiitemid = c.id" >> create.sql
-echo "where b.ApplicationId = 3" >> create.sql
-echo "and ItemStatus in ('closed', 'deleted')" >> create.sql
+echo "" >> branches.sql
+echo "select '../tools/delete_if_merged.bash ' + c.branch, c.branch, 'git push origin :' + c.branch, c.id" >> branches.sql
+echo "from rdiitem a" >> branches.sql
+echo "    inner join itemstatus b" >> branches.sql
+echo "        on a.StatusId = b.itemstatusid" >> branches.sql
+echo "    inner join @git c" >> branches.sql
+echo "        on a.rdiitemid = c.id" >> branches.sql
+echo "where b.ApplicationId = 3" >> branches.sql
+echo "and ItemStatus in ('closed', 'deleted')" >> branches.sql
