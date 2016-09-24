b856cfc8735c390e5b4b2dea051982caf5fbfd17
Benson Che <bche@resdat.com>
2016-09-23 17:09:32 -0700

    WIP time tracker

---
+++ chrome/time_tracker/scripts/main.jsx
@@ -0,0 +1,12 @@
+// var Task = React.createClass({
+//     render: function () {
+//         return (
+//             <div className="task">
+//                 <div>{this.props.name}</div>
+//                 <div>Time elapsed: { this.state.elapsed }</div>
+//             </div>
+//         );
+//     }
+// });
+
+// ReactDOM.render(<Task name='test' />, document.getElementById('main'));
