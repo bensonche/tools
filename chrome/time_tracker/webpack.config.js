5a7dafd74cb7d00bfd489ce34cc9d344525daedd
Benson Che <bche@resdat.com>
2016-10-06 08:43:57 -0700

    Reorg folder structure

--- chrome/time_tracker/webpack.config.js
+++ chrome/time_tracker/webpack.config.js
@@ -1,7 +1,7 @@
 var webpack = require("webpack");
 var path = require("path");
 
-var BUILD_DIR = path.resolve(__dirname, "scripts/");
+var BUILD_DIR = path.resolve(__dirname, "chrome/scripts/");
 var APP_DIR = path.resolve(__dirname, "src/");
 
 var config = {
