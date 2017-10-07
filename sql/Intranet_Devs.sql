111bc6dca7f51bd3ffce11054215ab5743156892
Benson Che <bche@resdat.com>
2017-10-07 01:13:56 -0700

    Hardcode in quinn and wilf

--- sql/Intranet_Devs.sql
+++ sql/Intranet_Devs.sql
@@ -1,7 +1,7 @@
 use rdi_production
 go
 
-select distinct a.empid, b.FULLNAME2, EMAIL
+select a.empid, b.FULLNAME2, EMAIL
 from time_sht a
 inner join AllUsers b
 on a.EMPID = b.USERID
@@ -10,4 +10,11 @@ and PROJECT_NO = 9
 and WK_DATE > DATEADD(ww, -2, GETDATE())
 and RDIItemId is not null
 and JOB_CODE in (340,345,430,435,305,310,311,1340,1345,1430,1435,1305,1310,1311)
+
+union
+
+select userid, fullname2, email
+from allusers
+where empid in (198, 582)
+
 order by FULLNAME2
