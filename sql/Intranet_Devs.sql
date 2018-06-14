044c274bad96eacc89e4a8a6550b6282ff0b7574
Benson Che <bche@resdat.com>
2018-06-13 17:30:00 -0700

    Add BA job code

--- sql/Intranet_Devs.sql
+++ sql/Intranet_Devs.sql
@@ -9,7 +9,7 @@ where a.CLIENT_ID = 363
 and PROJECT_NO = 9
 and WK_DATE > DATEADD(ww, -2, GETDATE())
 and RDIItemId is not null
-and JOB_CODE in (340,345,430,435,305,310,311, 502,1340,1345,1430,1435,1305,1310,1311, 502)
+and JOB_CODE in (340,345,430,435,305,310,311, 502,1340,1345,1430,1435,1305,1310,1311, 502, 332, 1332)
 
 union
 
@@ -18,3 +18,17 @@ from allusers
 where empid in (198, 582, 236)
 
 order by FULLNAME2
+
+/*
+
+select distinct a.empid, b.FULLNAME2, EMAIL
+from time_sht a
+inner join AllUsers b
+on a.EMPID = b.USERID
+where a.CLIENT_ID = 363
+and PROJECT_NO = 9
+and WK_DATE > DATEADD(ww, -2, GETDATE())
+and RDIItemId is null
+and JOB_CODE in (340,345,430,435,305,310,311, 502,1340,1345,1430,1435,1305,1310,1311, 502, 332, 1332)
+
+*/
