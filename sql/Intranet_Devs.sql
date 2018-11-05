dea4c95fb98f26a0750acd8d999c1715b2ec6abc
Benson Che <bche@resdat.com>
2018-11-05 09:30:09 -0800

    Remove duplicates in query 2

--- sql/Intranet_Devs.sql
+++ sql/Intranet_Devs.sql
@@ -19,7 +19,8 @@ where empid in (198, 236, 430)
 
 order by FULLNAME2
 
-/*
+
+-------------------------------------------------------------------
 
 select distinct a.empid, b.FULLNAME2, EMAIL
 from time_sht a
@@ -30,5 +31,25 @@ and PROJECT_NO = 9
 and WK_DATE > DATEADD(ww, -2, GETDATE())
 and RDIItemId is null
 and JOB_CODE in (340,345,430,435,305,310,311, 502,1340,1345,1430,1435,1305,1310,1311, 502, 332, 1332)
+and a.empid not in
+(
+	select a.empid
+	from time_sht a
+	inner join AllUsers b
+	on a.EMPID = b.USERID
+	where a.CLIENT_ID = 363
+	and PROJECT_NO = 9
+	and WK_DATE > DATEADD(ww, -2, GETDATE())
+	and RDIItemId is not null
+	and JOB_CODE in (340,345,430,435,305,310,311, 502,1340,1345,1430,1435,1305,1310,1311, 502, 332, 1332)
 
-*/
+	union
+
+	select userid
+	from allusers
+	where empid in (198, 236, 430)
+)
+
+
+order by b.FULLNAME2
+
